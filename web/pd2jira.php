4c9b245bbd3be9adf762bdea5d80acb76f05c634
Luke Epp <lucasfepp@gmail.com>
2016-06-19 14:04:49 -0700

    Fixed issue with jira password

--- web/pd2jira.php
+++ web/pd2jira.php
@@ -92,7 +92,7 @@ if ($messages) foreach ($messages->messages as $webhook) {
         post_to_jira($data, $base_url, $jira_username, $jira_password, $pd_subdomain, $incident_id, $note_verb, $jira_url, $pd_requester_id, $pd_api_token);
         // Call poll_pd_api with polling = true to start polling
         error_log('Calling poll api trigger');
-        call_poll_pd_api($pd_subdomain, $incident_id, $base_url, $jira_issue_id, true, $jira_username, $pd_api_token, $incident_number, $jira_transition_id);
+        call_poll_pd_api($pd_subdomain, $incident_id, $base_url, $jira_issue_id, true, $jira_username, $jira_password, $pd_api_token, $incident_number, $jira_transition_id);
       }
       // elseif ($verb == "resolve") {
       //   error_log('Incident resolved...');
@@ -111,7 +111,7 @@ if ($messages) foreach ($messages->messages as $webhook) {
   }
 }
 
-function call_poll_pd_api($pd_subdomain, $incident_id, $base_url, $jira_issue_id, $polling, $jira_username, $pd_api_token, $incident_number, $jira_transition_id) {
+function call_poll_pd_api($pd_subdomain, $incident_id, $base_url, $jira_issue_id, $polling, $jira_username, $jira_password, $pd_api_token, $incident_number, $jira_transition_id) {
   $data = array('polling'=>$polling,'pd_subdomain'=>$pd_subdomain,'incident_id'=>$incident_id,'base_url'=>$base_url,'jira_issue_id'=>$jira_issue_id,'jira_username'=>$jira_username,'pd_api_token'=>$pd_api_token,'jira_password'=>$jira_password,'incident_number'=>$incident_number,'jira_transition_id'=>$jira_transition_id);
   $data_json = json_encode($data);
   error_log('Pass before passing: ' . $data_json->jira_password);
